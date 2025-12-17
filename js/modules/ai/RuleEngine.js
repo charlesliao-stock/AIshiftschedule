6952c5fc5052fd1df85ba37c52157fcda0cf137d
charlesliao-stock <charlesliao.tw@gmail.com>
2025-12-17 16:59:11 +0800

    Update RuleEngine.js

--- js/modules/ai/RuleEngine.js
+++ js/modules/ai/RuleEngine.js
@@ -1,101 +1,69 @@
 export class RuleEngine {
 
-    /**
-     * 驗證單一員工
-     * @param {Object} staffConstraints 包含 calculatedMaxConsecutive, maxConsecutiveNights
-     */
-    static validateStaff(assignments, daysInMonth, shiftDefs, rules, staffConstraints = {}, lastMonthLastShift = 'OFF', lastMonthConsecutive = 0, checkUpToDay = null) {
+    static validateStaff(assignments, day, shiftDefs, rules, staffConstraints = {}, lastMonthLastShift = 'OFF', lastMonthConsecutive = 0) {
         const errors = {};
-        const safeAssignments = assignments || {};
-        const limitDay = checkUpToDay || daysInMonth;
-
-        // 1. 連續上班上限 (Spec 1 & 4)
-        // 優先順序: AutoScheduler 計算值(含長假) > 人員設定 > 單位設定 > 預設6
-        const maxConsecutive = staffConstraints.calculatedMaxConsecutive || staffConstraints.maxConsecutive || rules.maxConsecutiveWork || 6;
         
-        // 2. 連續夜班上限 (Spec 2)
-        // 若人員同意連夜(設定在 staffConstraints)，則使用該值；否則使用單位設定
-        const maxNight = staffConstraints.maxConsecutiveNights || rules.constraints?.maxConsecutiveNight || 4;
+        const currentShift = assignments[day] || 'OFF';
+        const isWorking = currentShift !== 'OFF' && currentShift !== 'M_OFF';
 
-        const isProtected = !!staffConstraints.isPregnant || !!staffConstraints.isPostpartum;
-        const minIntervalMins = (rules.constraints?.minInterval11h !== false) ? 660 : 0; 
+        if (!isWorking) return { errors };
 
-        let consecutiveDays = lastMonthConsecutive;
-        let consecutiveNights = (lastMonthLastShift === 'N') ? 1 : 0; 
-        let prevShift = lastMonthLastShift;
+        const maxConsecutive = staffConstraints.calculatedMaxConsecutive || rules.maxConsecutiveWork || 6;
+        const minIntervalMins = (rules.constraints?.minInterval11h !== false) ? 660 : 0;
 
-        // 建立時間查詢表
-        const shiftMap = {};
-        if (shiftDefs) {
-            shiftDefs.forEach(s => {
-                shiftMap[s.code] = {
-                    start: this._timeToMins(s.startTime),
-                    end: this._timeToMins(s.endTime)
-                };
-                if (shiftMap[s.code].end === 0) shiftMap[s.code].end = 1440;
-                if (shiftMap[s.code].end < shiftMap[s.code].start) shiftMap[s.code].end += 1440; 
-            });
+        // 1. 連續上班檢查
+        let consecutive = 0;
+        for (let d = day; d >= 1; d--) {
+            const s = assignments[d];
+            if (s && s !== 'OFF' && s !== 'M_OFF') consecutive++;
+            else break;
+        }
+        if (consecutive === day) {
+            consecutive += lastMonthConsecutive;
         }
 
-        for (let d = 1; d <= limitDay; d++) {
-            const shift = safeAssignments[d] || 'OFF';
-            const isWorking = shift !== 'OFF' && shift !== 'M_OFF';
-            const prevIsWorking = prevShift !== 'OFF' && prevShift !== 'M_OFF';
+        if (consecutive > maxConsecutive) {
+            errors[day] = `連${consecutive} (上限${maxConsecutive})`;
+            return { errors };
+        }
 
-            // A. 連續上班天數檢查
-            if (isWorking) consecutiveDays++; else consecutiveDays = 0;
-            if (consecutiveDays > maxConsecutive) {
-                errors[d] = `連${consecutiveDays} (上限${maxConsecutive})`;
+        // 2. 間隔 11 小時檢查
+        let prevShift = 'OFF';
+        if (day === 1) prevShift = lastMonthLastShift;
+        else prevShift = assignments[day - 1] || 'OFF';
+
+        const prevIsWorking = prevShift !== 'OFF' && prevShift !== 'M_OFF';
+
+        if (minIntervalMins > 0 && prevIsWorking) {
+            const shiftMap = {};
+            if (shiftDefs) {
+                shiftDefs.forEach(s => {
+                    shiftMap[s.code] = {
+                        start: this._timeToMins(s.startTime),
+                        end: this._timeToMins(s.endTime)
+                    };
+                    if (shiftMap[s.code].end === 0) shiftMap[s.code].end = 1440;
+                    if (shiftMap[s.code].end < shiftMap[s.code].start) shiftMap[s.code].end += 1440; 
+                });
             }
 
-            // B. 連續夜班檢查 (含人員意願)
-            if (shift === 'N') consecutiveNights++; else consecutiveNights = 0;
-            if (consecutiveNights > maxNight) {
-                errors[d] = `連夜${consecutiveNights} (上限${maxNight})`;
-            }
-
-            // C. 母性保護
-            if (isProtected && (shift === 'N' || shift === 'E')) {
-                errors[d] = "懷孕/哺乳不可夜班";
-            }
-
-            // D. 休息間隔 11 小時
-            if (minIntervalMins > 0 && prevIsWorking && isWorking) {
-                const t1 = shiftMap[prevShift];
-                const t2 = shiftMap[shift];
-                if (t1 && t2) {
-                    let interval = (1440 - t1.end) + t2.start;
-                    if (interval < minIntervalMins) {
-                        errors[d] = `間隔不足 (${Math.floor(interval/60)}h)`;
-                    }
+            const t1 = shiftMap[prevShift];
+            const t2 = shiftMap[currentShift];
+            
+            if (t1 && t2) {
+                let interval = (1440 - t1.end) + t2.start;
+                if (interval < minIntervalMins) {
+                    errors[day] = `間隔不足 (${Math.floor(interval/60)}h)`;
                 }
             }
+        }
 
-            // E. 大夜銜接 (防逆向)
-            if (shift === 'N') {
-                if (prevShift !== 'N' && prevShift !== 'OFF' && prevShift !== 'M_OFF') {
-                    errors[d] = "大夜前需OFF"; 
-                }
-            }
-
-            prevShift = shift;
+        // 3. 檢查大夜後是否接白/小
+        if (prevShift === 'N' && (currentShift === 'D' || currentShift === 'E')) {
+             errors[day] = "大夜後不可接白/小";
         }
 
         return { errors };
-    }
-
-    static validateAll(scheduleData, daysInMonth, staffList, unitSettings, rules) {
-        const staffReport = {};
-        const shiftDefs = unitSettings?.settings?.shifts || [];
-        staffList.forEach(staff => {
-            const result = this.validateStaff(
-                scheduleData.assignments[staff.uid], 
-                daysInMonth, shiftDefs, rules, staff.constraints,
-                'OFF', 0, null
-            );
-            if (Object.keys(result.errors).length > 0) staffReport[staff.uid] = result;
-        });
-        return { staffReport, coverageErrors: {} };
     }
 
     static _timeToMins(timeStr) {
