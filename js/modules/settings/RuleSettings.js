b6ed5948e88e813cb5ccb515b0f86b424e3c09b1
charlesliao-stock <charlesliao.tw@gmail.com>
2025-12-19 01:39:54 +0800

    Update RuleSettings.js

--- js/modules/settings/RuleSettings.js
+++ js/modules/settings/RuleSettings.js
@@ -17,13 +17,6 @@ export class RuleSettings {
         // 確保 strategyWeights 存在，如果沒有則使用預設 A 方案
         if (!this.unitSettings.strategyWeights) this.unitSettings.strategyWeights = AI_SCORING_CONFIG.STRATEGY_WEIGHTS.A; 
         if (!this.unitSettings.strategyPreset) this.unitSettings.strategyPreset = 'A';
-
-        // 修正：在資料載入完成後，強制重新渲染 Modal 內容
-        const modalContent = document.querySelector('#settings-modal .modal-content');
-        if (modalContent) {
-            modalContent.innerHTML = this.render();
-            this.attachEvents();
-        }
     }
 
     render() {
@@ -228,8 +221,11 @@ export class RuleSettings {
                 // 重新載入設定
                 await this.loadSettings();
                 // 重新渲染 Modal 內容
-                document.querySelector('#settings-modal .modal-content').innerHTML = this.render();
-                this.attachEvents();
+                const modalContent = document.querySelector('#settings-modal .modal-content');
+                if (modalContent) {
+                    modalContent.innerHTML = this.render();
+                    this.attachEvents();
+                }
             } catch (e) {
                 alert('儲存失敗: ' + e.message);
             }
